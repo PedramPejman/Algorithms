e5812dba76e217655ad23ea662f15cc6b8e0676b
Pedram Pejman <pedrampejman2010@gmail.com>
2015-01-25 14:19:10 -0800

    Homework 1 done

--- hw1/coinChange.cpp
+++ hw1/coinChange.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
 using namespace std;
 int coins [4]  = {25, 10, 5, 1};
@@ -19,7 +21,8 @@ void process (float amount) {
     return;
   }
   int index = 0;
-  printf("$%.2f ",amount);
+  setlocale(LC_NUMERIC, "");
+  printf("$%'.2f ",amount);
   amount -= floor(amount);
   amount = round (amount * 100);
   while (index < 4) {
@@ -29,11 +32,13 @@ void process (float amount) {
   }
   cout << endl;
 }
-int main() {
+int main(int argc, char** argv) {
+  if (argc < 2) exit(1);
+  FILE *file = fopen(argv[1], "r");
   float amount = 0;
-  do {
-  cin >> amount ;
-  process(amount);
-  } while (amount != -1.00);
+  while ((!feof(file))&&(amount != -1.00)) {
+    fscanf(file, "%f", &amount);
+    process(amount);
+  }
 }
 
