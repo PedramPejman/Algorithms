5a791e5cd2bcc676803793e5aa597e38896bdd59
Pedram Pejman <pedrampejman2010@gmail.com>
2015-02-19 12:35:20 -0800

    assy solution to hw4

--- hw4/Hw4.java
+++ hw4/Hw4.java
@@ -18,7 +18,8 @@ public class Hw4 {
               tempArr = temp.split(" ");
               points.add(new Point(tempArr[0], tempArr[1]));
             }
-            minDist = process(points);
+            //System.out.println("****************");
+            process(points);
             points = new ArrayList<Point>();
             n = Integer.parseInt(br.readLine());
       }
@@ -35,55 +36,54 @@ public class Hw4 {
   }
   minR = findMinDist(points, (beg+end)/2, end);
   minL = findMinDist(points, beg, (beg+end)/2);
-  //System.out.printf("*******In findMinDist - Range: (%d, %d)************\n",beg,end);
-  //System.out.printf("MinR: %f, MinL: %f\n",minR, minL );
   d = Math.min(minR, minL);
   return Math.min(d, findMidMin(points, beg, end, d));
  }
+
  public static double findMidMin(List<Point> points, int beg, int end, double d) { 
+    Set<Point> stripSet = new TreeSet<Point>();
     int mid = (beg + end)/2;
     int start = mid;
     int finish = mid;
-    while (d > distance(points.get(start) , points.get(mid))) start--;
-    while (d > distance(points.get(finish) , points.get(mid))) finish++;
-    for (int i=start; i<=finish; i++) {
-      for (int j=i+1; j<=Math.min(7,finish); j++) {
-        //System.out.printf("Min of (%d, %d) is between (%d:%s, %d:%s) = %f\n",start,finish,i,points.get(i),j,points.get(j),Math.min(d, distance(points.get(i), points.get(j))));
-        d = Math.min(d, distance(points.get(i), points.get(j)));
+    sortX = false;
+    while ((start > 0) && (d > Math.abs(points.get(mid).x - points.get(start).x))) stripSet.add(points.get(start--)); 
+    while ((finish < points.size()-1) && (d > Math.abs(points.get(mid).x - points.get(finish).x))) stripSet.add(points.get(finish++));
+    Point[] strip = stripSet.toArray(new Point[stripSet.size()]);
+    for (int i=0; i<=strip.length; i++) {
+      for (int j=i+1; j<=Math.min(i+7,strip.length-1); j++) {
+        d = Math.min(d, distance(strip[i], strip[j]));
       }
     }
-    //System.out.printf("*******In findMidMin - Range: (%d, %d) - midMin: %f************\n",start,finish,d);
     return d;
  }
+
  public static double minSquaredDist(List<Point> points, int beg, int end) {
-    double min = distance(points.get(beg),points.get(beg+1));
-    for (int i=beg; i<(end-1); i++) {
-      min = Math.min(min, distance(points.get(i), points.get(i+1)));
+    double min = distance(points.get(beg), points.get(beg+1));
+    if (end - beg == 3) {
+      min = Math.min(min, distance(points.get(beg), points.get(beg+2)));
+      min = Math.min(min, distance(points.get(beg+1), points.get(beg+2)));
     }
     return min;
  }
- public static double distance (Point a, Point b) {
-  return Math.sqrt(Math.pow(a.x-b.x,2) + Math.pow(a.y-b.y,2));
- }
- public static double process(List<Point> points) {
-     Collections.sort(points);
-     for (Point p:points) {
-        //System.out.println(p);
-     }
 
-     //System.out.println("min distance: "+ findMinDist(points, 0, points.size()));
-     cleanOutput(findMinDist(points,0,points.size()));
-     sortX = false; 
+ public static double distance (Point a, Point b) {
+  return Math.sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y));
+ }
+
+ public static void process(List<Point> points) {
+     sortX = true;
      Collections.sort(points);
      for (Point p:points) {
         //System.out.println(p);
      }
-  return 0.0;
- }
+     cleanOutput(findMinDist(points,0,points.size()));
+     }
+ 
  public static void cleanOutput(double output) {
   if (output>1000.0) System.out.println("infinity");
   else System.out.printf("%.4f\n",output);
  }
+
 static class Point implements Comparable<Point>{
   public double x,y;
   public Point(double x, double y) {
@@ -95,7 +95,7 @@ static class Point implements Comparable<Point>{
     this.y = Double.parseDouble(y);
   }
   public String toString() {
-    return this.x + " " + this.y;
+    return "(" + this.x + " " + this.y + ")";
   }
   public int compareTo(Point p) {
     if (Hw4.sortX) {
